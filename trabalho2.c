2b2ffd46a6b1ecf86741b7c25ac1f068413d2e3b
Pedro-Bat <104031353+Pedro-Bat@users.noreply.github.com>
2022-06-10 05:15:57 -0300

    Update trabalho2.c

--- trabalho2.c
+++ trabalho2.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Trabalho Prático IC1 Segundo Exercicio.
+//Aluno: Pedro Henrique Mendes de Lima Nº USP: 13823051
 
 void main()
 {
-    int n = 0, chute = 0, nmr = 0, volta = 1;
+    int i = 0, n = 0, chute = 0, nmr = 0, volta = 1; //Declarando variaveis
 
-   comeco:
-    printf("Jogo de advinhacao \n\nNesse jogo voce tem que acertar um numero gerado aleatoriamente de 1 a 1000. ");
-       
-        n = rand () % 1000;
+   comeco:// local onde o goto deve voltar
+    printf("Jogo de advinhacao \n\nNesse jogo voce tem que acertar um numero gerado aleatoriamente, digite o limite desse numero: ");
+       scanf("%d", i) // scaneia o limite para gerar um numero
+        n = rand () % i;
 
-        do{
+        do{ //Estrutura de repeticao para tentar novamente caso erre o valor
             printf("\nDe o seu chute: ");
-             scanf("%d", &chute);
+             scanf("%d", &chute); // escaneia a tentativa
 
-             if( chute<n)
+             if( chute<n) // verifica se a tentativa é maior ou menor que o numero gerado
              {
                  printf("Nao foi dessa vez, tente um numero maior\n");
-                 nmr += 1;
+                 nmr += 1;//soma os numeros de tentativas
              }
              if( chute>n)
               {
                  printf("Nao foi dessa vez, tente um numero menor\n");
                  nmr += 1;
               }
-        } while(chute != n);
+        } while(chute != n); 
         nmr +=1;
 
         printf("Parabens voce acertou.\nSeu numero de tentativas foi: %d\n", nmr);
         printf("Se quiser jogar novamente aperte o numero 0\n");
          scanf("%d", &volta);
 
-            if(volta == 0)
+            if(volta == 0)//reinicia o jogo
                 goto comeco;
             
 
