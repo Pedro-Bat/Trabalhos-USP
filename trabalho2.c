8a4706d7f45499b6a541bcfba2d2d0031b0160a1
Pedro-Bat <104031353+Pedro-Bat@users.noreply.github.com>
2022-06-25 06:34:07 -0300

    Update trabalho2.c

--- trabalho2.c
+++ trabalho2.c
@@ -4,13 +4,24 @@
 // Trabalho Prático IC1 Segundo Exercicio.
 //Aluno: Pedro Henrique Mendes de Lima Nº USP: 13823051
 
+/*
+Faça um programa para o “jogo de adivinhar um número”. O computador deve sortear
+um número entre 0 e 100 e pedir para o usuário tentar adivinhar este número. O usuário
+vai dizer o seu palpite, e o computador deve responder, se ele é maior ou menor que o
+número que ele sorteou. O programa termina somente quando o usuário acertar
+exatamente qual o número que o computador tinha sorteado, escrevendo uma mensagem
+de felicitações para o nosso usuário e indicando o número total de tentativas feitas.
+*/
+void clearScreen();
 void main()
 {
     int i = 0, n = 0, chute = 0, nmr = 0, volta = 1; //Declarando variaveis
 
    comeco:// local onde o goto deve voltar
+   
     printf("Jogo de advinhacao \n\nNesse jogo voce tem que acertar um numero gerado aleatoriamente, digite o limite desse numero: ");
-       scanf("%d", &i) // scaneia o limite para gerar um numero
+       scanf("%d", &i); // scaneia o limite para gerar um numero
+        nmr = 0;
         n = rand () % i;
 
         do{ //Estrutura de repeticao para tentar novamente caso erre o valor
@@ -22,7 +33,7 @@ void main()
                  printf("Nao foi dessa vez, tente um numero maior\n");
                  nmr += 1;//soma os numeros de tentativas
              }
-             if( chute>n)
+              if( chute>n)
               {
                  printf("Nao foi dessa vez, tente um numero menor\n");
                  nmr += 1;
@@ -30,12 +41,26 @@ void main()
         } while(chute != n); 
         nmr +=1;
 
-        printf("Parabens voce acertou.\nSeu numero de tentativas foi: %d\n", nmr);
+        printf("\nParabens voce acertou!.\nSeu numero de tentativas foi: %d\n", nmr);
         printf("Se quiser jogar novamente aperte o numero 0\n");
          scanf("%d", &volta);
 
             if(volta == 0)//reinicia o jogo
+            clearScreen();
                 goto comeco;
             
+
+}
+
+void clearScreen()
+{
+  #ifdef _WIN64
+    system("cls");
+  #elif __linux
+    system("clear");
+  #elif _WIN32
+    system("cls");
+  #else
+  #endif
 
 }
